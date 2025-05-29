f71c4d91457ae4bbbab75e936b48fd372bbd6189
MarieGi <giy.mariya@gmail.com>
2025-05-29 13:06:05 -0700

    add: scoped block service

---
+++ callable_service/callable_service_setup_block.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+# =============== Design ===============
+class BlockServiceBase
+  def self.call(...)
+    obj = new
+    yield(obj) if block_given?
+    obj.call(...)
+  end
+end
+
+# =============== Implementation ===============
+class BlockService < BlockServiceBase
+  def call
+    "Hello, #{name}"
+  end
+
+  private
+
+  attr_accessor :name
+end
+
+# Usage
+BlockService.call do |service|
+  service.name = 'Service'
+end
+# Output: 'Hello, Service'
+
+# =============== Testing ===============
+RSpec.describe BlockService do
+  let(:block_service) { BlockService }
+  let(:result) do
+    block_service.call do |service|
+      service.name = 'Service'
+    end
+  end
+
+  describe '#call' do
+    it 'returns "Hello, Service"' do
+      expect(result).to eq('Hello, Service')
+    end
+  end
+end
