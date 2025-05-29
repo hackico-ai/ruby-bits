70f3c548c85d80d0db1d14329b5f2866fbf59a3b
MarieGi <giy.mariya@gmail.com>
2025-05-29 13:23:50 -0700

    add: scoped incldue module class method

--- include_module_class_method/include_module_class_method.rb
+++ include_module_class_method/include_module_class_method.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# =============== Design ===============
 module IncludeModule
   def self.included(base)
     base.extend(ClassMethods)
@@ -7,7 +8,46 @@ module IncludeModule
 
   module ClassMethods
     def class_method
-      puts "class_method"
+      puts 'class_method'
+    end
+  end
+end
+
+# =============== Usage ===============
+class IncludeModuleClass
+  include IncludeModule
+end
+
+IncludeModuleClassMethod.class_method
+# Output: class_method
+
+
+# =============== Testing ===============
+# Unit: frankly, it's also contextual test
+#       but stubbed class for unit-like isolation behavior
+RSpec.describe IncludeModule do
+  let(:value) { 'class_method' }
+  stub_const('IncludeModuleClassMethod', Class.new do
+    include IncludeModule
+  end)
+
+  let(:included_klass) { IncludeModuleClassMethod }
+
+  describe '#class_method' do
+    it 'returns "class_method"' do
+      expect(included_klass.class_method).to eq(value)
+    end
+  end
+end
+
+# Integration: actually class behavi
+RSpec.describe IncludeModuleClass do
+  let(:value) { 'class_method' }
+  let(:included_klass) { IncludeModuleClass }
+
+  describe '#class_method' do
+    it 'returns "class_method"' do
+      expect(included_klass.class_method).to eq(value)
     end
   end
 end
